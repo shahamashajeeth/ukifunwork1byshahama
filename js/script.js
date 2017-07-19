6d470de101c36395e8ddf592f8c85dde56c6dc46
ukistu08 <sfshahama145@gmail.com>
2017-07-19 14:34:41 +0530

    add

---
+++ js/script.js
@@ -0,0 +1,78 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+    SC.stream('/tracks/154550407',function(sound){
+         $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/290209423',function(sound){
+         $('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/307049640',function(sound){
+         $('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/267806421',function(sound){
+         $('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/300585988',function(sound){
+         $('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/246782752',function(sound){
+         $('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+});
